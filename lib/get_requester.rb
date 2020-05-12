7423a6978a0204db87a4c234f292bc79cab9ea4e
Luke Chen <luke.chen.2010@gmail.com>
2020-05-12 15:44:52 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,6 +1,6 @@
 class GetRequester
-    def get_programs
-      uri = URI.parse(URL)
+    def get_requester(url)
+      uri = URI.parse(url)
       response = Net::HTTP.get_response(uri)
       response.body
     end
